2abb7ce41c70b60dc6b6967d023dd12f8070db3f
Micah Reich <micahreich@MacBook-Pro-6.local>
2022-08-23 02:35:51 -0400

    aah

--- src/dataExplorer.cpp
+++ src/dataExplorer.cpp
@@ -2,14 +2,21 @@
 #include <fstream>
 #include <dirent.h>
 #include <errno.h>
-#include <string>
 #include <iostream>
 #include <vector>
+#include <regex>
+#include <tuple>
+#include <string>
+#include <chrono>
 
 using namespace std;
 
 string moduleName = "[DataExplorer] ";
 int STATUS_CODE = 0;
+
+bool DataExplorer::substring(string s1, string s2) {
+    return s2.find(s1) != string::npos;
+}
 
 DataExplorer::DataExplorer(string _dataDirectory) {
     string* requiredFiles[4] = {&proteinLinksPath, &proteinAnnotationsPath, &proteinSequencesPath, &clusterSizesPath};
@@ -25,10 +32,12 @@ DataExplorer::DataExplorer(string _dataDirectory) {
         cout << moduleName << "Retrieving data from " << dataDirectory << "..." << endl;
     }
 
+    cout << string(80, '.') << endl;
+
+    // iterate through data directory, assign file paths to appropriate data depending on names
     while((dirp = readdir(dp)) != NULL) {
         string currFileName = string(dirp->d_name);
         
-        // iterate through data directory, assign file paths to appropriate data depending on names
         if(substring("protein.links", currFileName)) {
             proteinLinksPath = dataDirectory + "/" + currFileName;
             cout << moduleName << "Found: " <<  proteinLinksPath << " !" << endl;
@@ -51,12 +60,237 @@ DataExplorer::DataExplorer(string _dataDirectory) {
             STATUS_CODE = 1;
         }
     }
+
+    // populate protein links
+    cout << string(80, '.') << endl;
+
+    auto start = chrono::high_resolution_clock::now();
+    cout << moduleName << "Populating protein links..." << endl;
+
+    // populateProteinLinks();
+
+    auto stop = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::milliseconds>(stop - start).count() / 1e3;
+    cout << moduleName << "Finished! Took " << duration << " seconds " << endl;
+
+    // populate protein annotations
+    start = chrono::high_resolution_clock::now();
+    cout << moduleName << "Populating protein annotations..." << endl;
+    
+    // populateProteinAnnotations();
+
+    stop = chrono::high_resolution_clock::now();
+    duration = chrono::duration_cast<chrono::milliseconds>(stop - start).count() / 1e3;
+    cout << moduleName << "Finished! Took " << duration << " seconds " << endl;
+
+    // populate protein sequences
+    start = chrono::high_resolution_clock::now();
+    cout << moduleName << "Populating protein AA sequences..." << endl;
+    
+    // populateProteinSequences();
+
+    stop = chrono::high_resolution_clock::now();
+    duration = chrono::duration_cast<chrono::milliseconds>(stop - start).count() / 1e3;
+    cout << moduleName << "Finished! Took " << duration << " seconds " << endl;
+
+    // populate cluster sizes
+    start = chrono::high_resolution_clock::now();
+    cout << moduleName << "Populating cluster sizes..." << endl;
+    
+    populateClusterSizes();
+
+    stop = chrono::high_resolution_clock::now();
+    duration = chrono::duration_cast<chrono::milliseconds>(stop - start).count() / 1e3;
+    cout << moduleName << "Finished! Took " << duration << " seconds " << endl;
 }
 
-bool DataExplorer::substring(string s1, string s2) {
-    return s2.find(s1) != string::npos;
+void DataExplorer::populateProteinLinks() {
+    ifstream file(proteinLinksPath);
+
+    if(file.is_open()) {
+        int cnt = 0;
+
+        string line;
+        const string delim = " ";
+
+        vector<string> tokenizedLine(3);
+
+        while(getline(file, line)) {
+            size_t pos = 0;
+            size_t numTokens = 0;
+
+            tokenizedLine = vector<string>{"", "", ""};
+            
+            while((pos = line.find(delim)) != string::npos) {
+                string token = line.substr(0, pos);
+                tokenizedLine[numTokens] = token;
+                ++numTokens;
+
+                line.erase(0, pos + delim.length());
+            }
+            tokenizedLine[2] = line; // set final token to the link score
+
+            // verify that the tokenized line is a correct representation of a protein link
+            string currProtein1, currProtein2;
+            int currScore;
+
+            if(regex_match(tokenizedLine[0], proteinNameRegex) &&
+               regex_match(tokenizedLine[1], proteinNameRegex) &&
+               regex_match(tokenizedLine[2], proteinScoreRegex)) {
+                currProtein1 = tokenizedLine[0];
+                currProtein2 = tokenizedLine[1];
+                currScore =  stoi(tokenizedLine[2]);
+
+                if(proteinLinks.find(currProtein1) != proteinLinks.end()) {
+                    // extend entry in the hashmap
+                    proteinLinks[currProtein1].push_back(make_tuple(currProtein2, currScore));
+                } else {
+                    // make a new entry in the hashmap
+                    proteinLinks[currProtein1] = vector<tuple <string,int> >{
+                        make_tuple(currProtein2, currScore)
+                    };
+                }
+            }            
+        }
+        file.close();
+    }
+}
+
+void DataExplorer::populateProteinAnnotations() {
+
+    ifstream file(proteinAnnotationsPath);
+
+    if(file.is_open()) {
+        string line;
+        const string delim = " ";
+
+        vector<string> tokenizedLine(3);
+
+        while(getline(file, line)) {
+            size_t pos = 0;
+            size_t numTokens = 0;
+
+            tokenizedLine = vector<string>{"", "", ""};
+            
+            while((pos = line.find(delim)) != string::npos) {
+                string token = line.substr(0, pos);
+                tokenizedLine[numTokens] = token;
+                ++numTokens;
+
+                line.erase(0, pos + delim.length());
+            }
+            tokenizedLine[2] = line; // set final token to the link score
+
+            // verify that the tokenized line is a correct representation of a protein link
+            string currProtein, currCluster;
+
+            if(regex_match(tokenizedLine[1], clusterIDRegex) &&
+               regex_match(tokenizedLine[2], proteinNameRegex)) {
+
+                currProtein = tokenizedLine[2];
+
+                size_t colonDelimIdx = tokenizedLine[1].find(":");
+                currCluster = tokenizedLine[1].substr(colonDelimIdx + 1, 
+                                                      tokenizedLine[1].length() - (colonDelimIdx + 1));
+
+                cout << currProtein << " " << currCluster << endl;
+                if(proteinAnnotations.find(currProtein) != proteinAnnotations.end()) {
+                    // extend entry in the hashmap
+                    proteinAnnotations[currProtein].push_back(currCluster);
+                } else {
+                    // make a new entry in the hashmap
+                    proteinAnnotations[currProtein] = vector<string>{ currCluster };
+                }
+            }
+        }
+        file.close();
+    }
+}
+
+void DataExplorer::populateProteinSequences() {
+    ifstream file(proteinSequencesPath);
+
+    if(file.is_open()) {
+        string line;
+        const string delim = " ";
+
+        vector<string> tokenizedLine(2);
+
+        while(getline(file, line)) {
+            size_t pos = 0;
+            size_t numTokens = 0;
+
+            tokenizedLine = vector<string>{"", ""};
+            
+            while((pos = line.find(delim)) != string::npos) {
+                string token = line.substr(0, pos);
+                tokenizedLine[numTokens] = token;
+                ++numTokens;
+
+                line.erase(0, pos + delim.length());
+            }
+            tokenizedLine[1] = line; // set final token to the link score
+
+            // verify that the tokenized line is a correct representation of a protein link
+            string currProtein, currSequence;
+
+            if(regex_match(tokenizedLine[0], proteinNameRegex) &&
+               regex_match(tokenizedLine[1], aminoAcidRegex)) {
+                currProtein = tokenizedLine[0];
+                currSequence = tokenizedLine[1];
+
+                // make a new entry in the hashmap
+                proteinSequences[currProtein] = currSequence;
+            }
+        }
+        file.close();
+    }
+}
+
+void DataExplorer::populateClusterSizes() {
+    ifstream file(clusterSizesPath);
+
+    if(file.is_open()) {
+        string line;
+        const string delim = " ";
+
+        vector<string> tokenizedLine{};
+
+        while(getline(file, line)) {
+            size_t pos = 0;
+            size_t numTokens = 0;
+
+            tokenizedLine = vector<string>{};
+            
+            while((pos = line.find(delim)) != string::npos) {
+                string token = line.substr(0, pos);
+                tokenizedLine.push_back(token);
+                ++numTokens;
+
+                line.erase(0, pos + delim.length());
+            }
+            tokenizedLine.push_back(line); // set final token to the link score
+
+            // verify that the tokenized line is a correct representation of a protein link
+            string currCluster;
+            int currClusterSize;
+
+            if(regex_match(tokenizedLine[1], clusterIDRegex) &&
+               regex_match(tokenizedLine[2], clusterSizeRegex)) {
+                currClusterSize = stoi(tokenizedLine[2]);
+
+                size_t colonDelimIdx = tokenizedLine[1].find(":");
+                currCluster = tokenizedLine[1].substr(colonDelimIdx + 1, 
+                                                      tokenizedLine[1].length() - (colonDelimIdx + 1));
+
+                // make a new entry in the hashmap
+                clusterSizes[currCluster] = currClusterSize;
+            }
+        }
+        file.close();
+    }
 }
 
 int main() {
-    DataExplorer d("data/Ecoli");
+    DataExplorer d("data/ecoli");
 }
--- src/dataExplorer.h
+++ src/dataExplorer.h
@@ -8,31 +8,45 @@
 #include <iostream>
 #include <vector>
 #include <map>
+#include <tuple>
+#include <regex>
 
 using namespace std;
 
 class DataExplorer {
     string dataDirectory;
 
+    const regex proteinNameRegex = regex("([0-9]+)(.)([a-zA-Z0-9]+)(_)[0-9]+");
+    const regex proteinScoreRegex = regex("[0-9][0-9][0-9]([0-9]?)");
+    const regex clusterIDRegex = regex("(CL):([0-9])+");
+    const regex clusterSizeRegex = regex("[0-9]+");
+    const regex aminoAcidRegex = regex("[A-Z]+");
+    
+
     string proteinLinksPath = "proteinLinksPath";
-    map<string, vector<string> > proteinLinks;
+    map<string, vector< tuple<string, int> > > proteinLinks;
+    void populateProteinLinks();
 
     string proteinAnnotationsPath = "proteinAnnotationsPath";
     map<string, vector<string> > proteinAnnotations;
+    void populateProteinAnnotations();
 
     string proteinSequencesPath = "proteinSequencesPath";
     map<string, string> proteinSequences;
+    void populateProteinSequences();
 
     string clusterSizesPath = "clusterSizesPath";
     map<string, int> clusterSizes;
+    void populateClusterSizes();
 
     bool substring(string s1, string s2);
+    void timedFunction(DataExplorer* ob, void (DataExplorer::*fn)());
 
     public:
         DataExplorer(string _dataDirectory);
 
         // define static getter methods
-        map<string, vector<string> > getProteinLinks() {return proteinLinks;}
+        map<string, vector< tuple<string,int> > > getProteinLinks() {return proteinLinks;}
         map<string, vector<string> > getProteinAnnotations() {return proteinAnnotations;}
         map<string, string> getProteinSequences() {return proteinSequences;}
         map<string, int> getClusterSizes() {return clusterSizes;}
